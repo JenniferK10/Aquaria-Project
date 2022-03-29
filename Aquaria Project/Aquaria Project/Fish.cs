24f5abc578f0fc7337665a036ac6a0cf9b0c0535
Florence Lourdes <florence.lourdes@snhu.edu>
2022-03-29 17:08:50 -0400

    Added Fish Class

---
+++ Aquaria Project/Aquaria Project/Fish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquaria_Project
+{
+    internal class Fish
+    {
+        String name;
+        public Fish(String name)
+        {
+            this.name = name;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
