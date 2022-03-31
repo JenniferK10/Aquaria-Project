f5e6a7d680b70a66922a5641a6811e15855a341a
suhalip <99282369+suhalip@users.noreply.github.com>
2022-03-31 15:22:24 -0400

    added huntingGame

--- Aquaria Project/Aquaria Project/huntingGameForm.Designer.cs
+++ Aquaria Project/Aquaria Project/huntingGameForm.Designer.cs
@@ -143,6 +143,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
             this.ClientSize = new System.Drawing.Size(1028, 594);
             this.Controls.Add(this.lab_score);
             this.Controls.Add(this.lab_hunt);
