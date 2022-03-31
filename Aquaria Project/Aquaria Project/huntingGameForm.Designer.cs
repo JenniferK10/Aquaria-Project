d7327022cd48cd96910e81062a3908db1fcf0f18
suhalip <99282369+suhalip@users.noreply.github.com>
2022-03-31 15:08:55 -0400

    added huntingGame

--- Aquaria Project/Aquaria Project/huntingGameForm.Designer.cs
+++ Aquaria Project/Aquaria Project/huntingGameForm.Designer.cs
@@ -28,20 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(huntingGameForm));
+            System.Windows.Forms.Timer timer;
+            this.fish1 = new System.Windows.Forms.PictureBox();
+            this.fish2 = new System.Windows.Forms.PictureBox();
+            this.fish3 = new System.Windows.Forms.PictureBox();
+            this.fish4 = new System.Windows.Forms.PictureBox();
+            this.lab_over = new System.Windows.Forms.Label();
+            this.lab_miss = new System.Windows.Forms.Label();
+            this.lab_hunt = new System.Windows.Forms.Label();
+            this.lab_score = new System.Windows.Forms.Label();
+            timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fish1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // fish1
+            // 
+            this.fish1.BackColor = System.Drawing.Color.Transparent;
+            this.fish1.Image = ((System.Drawing.Image)(resources.GetObject("fish1.Image")));
+            this.fish1.Location = new System.Drawing.Point(630, 464);
+            this.fish1.Name = "fish1";
+            this.fish1.Size = new System.Drawing.Size(156, 85);
+            this.fish1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fish1.TabIndex = 0;
+            this.fish1.TabStop = false;
+            this.fish1.Click += new System.EventHandler(this.fish1_Click);
+            // 
+            // fish2
+            // 
+            this.fish2.BackColor = System.Drawing.Color.Transparent;
+            this.fish2.Image = ((System.Drawing.Image)(resources.GetObject("fish2.Image")));
+            this.fish2.Location = new System.Drawing.Point(57, 347);
+            this.fish2.Name = "fish2";
+            this.fish2.Size = new System.Drawing.Size(184, 157);
+            this.fish2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fish2.TabIndex = 1;
+            this.fish2.TabStop = false;
+            this.fish2.Click += new System.EventHandler(this.fish2_Click);
+            // 
+            // fish3
+            // 
+            this.fish3.BackColor = System.Drawing.Color.Transparent;
+            this.fish3.Image = ((System.Drawing.Image)(resources.GetObject("fish3.Image")));
+            this.fish3.Location = new System.Drawing.Point(603, 185);
+            this.fish3.Name = "fish3";
+            this.fish3.Size = new System.Drawing.Size(270, 157);
+            this.fish3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fish3.TabIndex = 2;
+            this.fish3.TabStop = false;
+            this.fish3.Click += new System.EventHandler(this.fish3_Click);
+            // 
+            // fish4
+            // 
+            this.fish4.Image = ((System.Drawing.Image)(resources.GetObject("fish4.Image")));
+            this.fish4.Location = new System.Drawing.Point(86, 12);
+            this.fish4.Name = "fish4";
+            this.fish4.Size = new System.Drawing.Size(184, 157);
+            this.fish4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fish4.TabIndex = 3;
+            this.fish4.TabStop = false;
+            this.fish4.Click += new System.EventHandler(this.fish4_Click);
+            // 
+            // lab_over
+            // 
+            this.lab_over.AutoSize = true;
+            this.lab_over.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_over.Location = new System.Drawing.Point(389, 238);
+            this.lab_over.Name = "lab_over";
+            this.lab_over.Size = new System.Drawing.Size(296, 56);
+            this.lab_over.TabIndex = 4;
+            this.lab_over.Text = "Game Over!";
+            this.lab_over.Visible = false;
+            // 
+            // lab_miss
+            // 
+            this.lab_miss.AutoSize = true;
+            this.lab_miss.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_miss.Location = new System.Drawing.Point(-4, -1);
+            this.lab_miss.Name = "lab_miss";
+            this.lab_miss.Size = new System.Drawing.Size(245, 56);
+            this.lab_miss.TabIndex = 5;
+            this.lab_miss.Text = "Missed: 0";
+            // 
+            // lab_hunt
+            // 
+            this.lab_hunt.AutoSize = true;
+            this.lab_hunt.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_hunt.Location = new System.Drawing.Point(257, -1);
+            this.lab_hunt.Name = "lab_hunt";
+            this.lab_hunt.Size = new System.Drawing.Size(246, 56);
+            this.lab_hunt.TabIndex = 6;
+            this.lab_hunt.Text = "Hunted: 0";
+            // 
+            // lab_score
+            // 
+            this.lab_score.AutoSize = true;
+            this.lab_score.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_score.Location = new System.Drawing.Point(495, -1);
+            this.lab_score.Name = "lab_score";
+            this.lab_score.Size = new System.Drawing.Size(215, 56);
+            this.lab_score.TabIndex = 7;
+            this.lab_score.Text = "Score: 0";
+            // 
+            // timer
+            // 
+            timer.Interval = 30;
+            timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // huntingGameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1028, 594);
+            this.Controls.Add(this.lab_score);
+            this.Controls.Add(this.lab_hunt);
+            this.Controls.Add(this.lab_miss);
+            this.Controls.Add(this.lab_over);
+            this.Controls.Add(this.fish4);
+            this.Controls.Add(this.fish3);
+            this.Controls.Add(this.fish2);
+            this.Controls.Add(this.fish1);
             this.Name = "huntingGameForm";
             this.Text = "huntingGameForm";
-            this.Load += new System.EventHandler(this.huntingGameForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.huntingGameForm_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.fish1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox fish1;
+        private System.Windows.Forms.PictureBox fish2;
+        private System.Windows.Forms.PictureBox fish3;
+        private System.Windows.Forms.PictureBox fish4;
+        private System.Windows.Forms.Label lab_over;
+        private System.Windows.Forms.Label lab_miss;
+        private System.Windows.Forms.Label lab_hunt;
+        private System.Windows.Forms.Label lab_score;
     }
 }
--- Aquaria Project/Aquaria Project/huntingGameForm.cs
+++ Aquaria Project/Aquaria Project/huntingGameForm.cs
@@ -15,11 +15,100 @@ namespace Aquaria_Project
         public huntingGameForm()
         {
             InitializeComponent();
+            //change the cursor
+            /*Bitmap bmp = new Bitmap(Properties.Resources.cursor);
+            Cursor crcs = new Cursor(bmp.GetHbitmap());
+            this.Cursor = crcs;*/
+        }
+        int miss, hunt, score;
+        void game_Status()
+        {
+            if (lab_miss.Text == "Missed: 5")
+            {
+                lab_over.Visible = true;
+                timer.Stop();
+            } // game over is 5 fish missed
+            if (lab_hunt.Text == "Hunted: 10")// game over is 10 fish is hunted
+            {
+                lab_over.Visible = true;
+                timer.Stop();
+            }
         }
 
-        private void huntingGameForm_Load(object sender, EventArgs e)
+        private void fish1_Click(object sender, EventArgs e)
         {
+            hunt++;
+            lab_hunt.Text = "Hunted: " + hunt;
+            Image clown = Image.FromFile("fire.gif");
+            score += 2;
+            lab_score.Text = "Score: " + score;
+        }
 
+        private void fish2_Click(object sender, EventArgs e)
+        {
+            hunt++;
+            lab_hunt.Text = "Hunted: " + hunt;
+            Image clown = Image.FromFile("fire.gif");
+            score += 5;
+            lab_score.Text = "Score: " + score;
+        }
+
+        private void fish3_Click(object sender, EventArgs e)
+        {
+            hunt++;
+            lab_hunt.Text = "Hunted: " + hunt;
+            Image clown = Image.FromFile("fire.gif");
+            score += 5;
+            lab_score.Text = "Score: " + score;
+        }
+
+        private void fish4_Click(object sender, EventArgs e)
+        {
+            hunt++;
+            lab_hunt.Text = "Hunted: " + hunt;
+            Image clown = Image.FromFile("fire.gif");
+            score += 5;
+            lab_score.Text = "Score: " + score;
+        }
+
+        private void huntingGameForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            miss++;
+            lab_miss.Text = "Missed: " + miss;// if the user miss the target 
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            fish();
+            game_Status();
+        }
+
+        void fish() // add all the fish images and make them move
+        {
+            if (fish1.Left < 0)
+            {
+                fish1.Left = 1000;
+                Image clown = Image.FromFile("clown.gif");
+            }
+            if (fish2.Left < 0)
+            {
+                fish2.Left = 1000;
+                Image clown = Image.FromFile("fish11.gif");
+            }
+            if (fish3.Left < 0)
+            {
+                fish3.Left = 1000;
+                Image clown = Image.FromFile("crop.png");
+            }
+            if (fish4.Left < 0)
+            {
+                fish4.Left = 1000;
+                Image clown = Image.FromFile("fish5.gif");
+            }
+            fish1.Left -= 5;
+            fish2.Left -= 10;
+            fish3.Left -= 15;
+            fish4.Left -= 13;
         }
     }
 }
